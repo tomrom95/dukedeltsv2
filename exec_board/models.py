556ec1457a1aec97590a87bf98fc0b1167605505
Tommy <tomrom95@gmail.com>
2015-07-15 18:50:56 -0400

    Added contact section and footer

--- exec_board/models.py
+++ exec_board/models.py
@@ -6,6 +6,7 @@ class BoardMember(models.Model):
 	year = models.CharField(max_length=50, choices=(
     	('2015','2015'), ('2016','2016'), ('2017', '2017'),
     	('2018', '2018'), ('2019', '2019')))
+	email = models.EmailField()
 	pic = models.ImageField()
 
 	def __unicode__(self):
